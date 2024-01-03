044f984682cbf723e5eae742dd1c5c149e4b8ac2
Jack Pickard <jack@hertilityhealth.com>
2024-01-03 11:49:20 +0000

    more auth hiding

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,11 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { UserNav } from "./UserNav";
+import { useSession } from "next-auth/react";
 
 export const NavBar: React.FC = () => {
+  const session = useSession();
+  const isAuth = session.status === "authenticated";
   return (
     <div className="container flex flex-row items-center justify-around gap-2 py-8 sm:px-20 ">
       <div className="flex flex-col gap-2">
@@ -11,12 +14,16 @@ export const NavBar: React.FC = () => {
         </h3>
         <div>Get off your arse</div>
       </div>
-      <Button variant="link" asChild>
-        <Link className="text-lg" href="/summary">
-          Summary
-        </Link>
-      </Button>
-      <UserNav />
+      {isAuth && (
+        <>
+          <Button variant="link" asChild>
+            <Link className="text-lg" href="/summary">
+              Summary
+            </Link>
+          </Button>
+          <UserNav />
+        </>
+      )}
     </div>
   );
 };
