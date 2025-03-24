80177a0f130b55d65d8f1062473eb3fb73362975
Jack Pickard <jack@hertilityhealth.com>
2025-03-24 10:15:30 +0000

    come on!

--- src/server/initScheduledJobs.ts
+++ src/server/initScheduledJobs.ts
@@ -1,5 +1,5 @@
 import { setupScheduledJobs } from "./scheduled-jobs";
 
 console.log("Starting scheduled jobs service...");
-setupScheduledJobs();
+void setupScheduledJobs();
 console.log("Scheduled jobs service running. Press Ctrl+C to exit.");
